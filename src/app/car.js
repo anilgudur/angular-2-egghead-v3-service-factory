019f93ca95c205eea35cbe420160de0840ad807c
Anil <anil.gudur@clariontechnologies.co.in>
2017-03-07 20:28:25 +0530

    v3-01-egghead-angular-2-what-is-dependency-injection-b9f0c87e complete

--- src/app/car.js
+++ src/app/car.js
@@ -1,10 +1,8 @@
 "use strict";
-var engine_1 = require('./engine');
-var doors_1 = require('./doors');
 var Car = (function () {
-    function Car() {
-        this.engine = new engine_1.Engine;
-        this.doors = new doors_1.Doors;
+    function Car(engine, doors) {
+        this.engine = engine;
+        this.doors = doors;
     }
     Car.prototype.startEngine = function () {
         this.engine.start();
--- src/app/car.main.js
+++ src/app/car.main.js
@@ -1,7 +1,11 @@
 "use strict";
+var engine_1 = require('./engine');
+var doors_1 = require('./doors');
 var car_1 = require('./car');
 function main() {
-    var car = new car_1.Car();
+    var engine = new engine_1.Engine;
+    var doors = new doors_1.Doors;
+    var car = new car_1.Car(engine, doors);
     car.startEngine();
 }
 main();
